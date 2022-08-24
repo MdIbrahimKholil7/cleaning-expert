b05951410e08b67f7f59892e2e8d3c2ce57aecbe
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-08-24 20:54:04 +0600

    timeline design add

--- src/components/Pages/TimeLine.js
+++ src/components/Pages/TimeLine.js
@@ -6,6 +6,7 @@ import icon2 from '../../assets/icon2.png'
 import icon3 from '../../assets/icon3.png'
 import icon4 from '../../assets/icon4.png'
 import { makeStyles } from '@material-ui/styles';
+import { Container, fontWeight } from '@mui/system';
 
 const useStyles = makeStyles({
     imgContainer: {
@@ -17,7 +18,12 @@ const useStyles = makeStyles({
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        marginBottom: '5px'
+        marginBottom: '5px',
+        marginTop: '5px',
+        '@media (max-width:500px)': {
+            width: '20px',
+            height: '20px',
+        },
     },
     img: {
         width: '100%',
@@ -27,9 +33,35 @@ const useStyles = makeStyles({
         height: '50px',
         background: '#7575754D',
         width: '4px'
+    },
+    timeLine:{
+        width:'100vw !important'
     }
 })
 
+const workTitle = {
+
+    xs: '16px',
+    md: '18px',
+    lg: '22px'
+
+}
+
+const font = {
+
+    xs: '12px',
+    md: '15px',
+    lg: '18px'
+
+}
+
+const font1 = {
+
+    xs: '11px',
+    md: '13px',
+    lg: '16px'
+
+}
 
 const TimeLine = () => {
     const classes = useStyles()
@@ -39,124 +71,169 @@ const TimeLine = () => {
                 <Box
                     textAlign='center'
                 >
-                    <Typography>
+                    <Typography
+                        fontSize={
+                            workTitle
+                        }
+                        sx={{
+                            color: '#025B5D',
+                            fontWeight: 'bold',
+
+                            marginBottom: '7px'
+                        }}
+                    >
                         How it Works
                     </Typography>
-                    <Divider
-                        sx={{ width: '130px', margin: '20px 0' }}
-                    />
+                    <Box
+                        sx={{
+
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Divider
+                            sx={{
+                                width: '80px',
+                                background: '#757575',
+                                height: '3px'
+                            }}
+                        />
+                    </Box>
                 </Box>
 
-                <Box>
-                    <Timeline
-                        sx={{ height: '500px' }}
+                <Box
+                    sx={{
+
+                        display: 'flex',
+                        justifyContent: 'start'
+                    }}
+                >
+                    <Container
+                        maxWidth='lg'
+                        sx={{
+
+                            display: 'flex',
+                            justifyContent: 'start',
+                            marginLeft:'-50px',
+                            '@media (max-width:500px)': {
+                                marginLeft: '-140px',
+                            },
+                        }}
                     >
-                        <TimelineItem>
-                            <TimelineSeparator    >
+                        <Timeline
+                        // className={classes.timeLine}
+                        align='left'
+                        >
+                            <TimelineItem>
+                                <TimelineSeparator    >
 
-                                <Box className={classes.imgContainer}>
-                                    <img className={classes.img} src={icon1} alt="" />
-                                </Box>
-                                <TimelineConnector
-                                    className={classes.connector}
-                                />
-                            </TimelineSeparator>
-                            <TimelineContent>
-                                <Typography
-                                    variant='h5'
-                                    contents='h5'
-                                    fontSize='18px'
-                                    fontWeight='bold'
+                                    <Box className={classes.imgContainer}>
+                                        <img className={classes.img} src={icon2} alt="" />
+                                    </Box>
+                                    <TimelineConnector
+                                        className={classes.connector}
+                                    />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <Typography
+                                        variant='h5'
+                                        contents='h5'
+                                        fontSize={font}
+                                        fontWeight='bold'
 
-                                >Choose the Appliances</Typography>
-                                <Typography
-                                    variant='p'
-                                    contents='p'
-                                    fontSize='16px'
-                                    color='#757575'
-                                >Select the Service required</Typography>
-                            </TimelineContent>
+                                    >Choose the Appliances</Typography>
+                                    <Typography
+                                        variant='p'
+                                        contents='p'
+                                        fontSize={font1}
+                                        color='#757575'
+                                    >Select the Service required</Typography>
+                                </TimelineContent>
 
-                        </TimelineItem>
-                        <TimelineItem>
-                            <TimelineSeparator    >
+                            </TimelineItem>
+                            <TimelineItem>
+                                <TimelineSeparator    >
 
-                                <Box className={classes.imgContainer}>
-                                    <img className={classes.img} src={icon1} alt="" />
-                                </Box>
-                                <TimelineConnector
-                                    className={classes.connector}
-                                />
-                            </TimelineSeparator>
-                            <TimelineContent>
-                                <Typography
-                                    variant='h5'
-                                    contents='h5'
-                                    fontSize='18px'
-                                    fontWeight='bold'
+                                    <Box className={classes.imgContainer}>
+                                        <img className={classes.img} src={icon4} alt="" />
+                                    </Box>
+                                    <TimelineConnector
+                                        className={classes.connector}
+                                    />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <Typography
+                                        variant='h5'
+                                        contents='h5'
+                                        fontSize={font}
+                                        fontWeight='bold'
 
-                                >Choose the Appliances</Typography>
-                                <Typography
-                                    variant='p'
-                                    contents='p'
-                                    fontSize='16px'
-                                    color='#757575'
-                                >Select the Service required</Typography>
-                            </TimelineContent>
-                        </TimelineItem>
-                        <TimelineItem>
-                            <TimelineSeparator    >
+                                    >
+                                        Choose your time-slot
+                                    </Typography>
+                                    <Typography
+                                        variant='p'
+                                        contents='p'
+                                        fontSize={font1}
+                                        color='#757575'
+                                    >We serve from 9am-9pm</Typography>
+                                </TimelineContent>
+                            </TimelineItem>
+                            <TimelineItem>
+                                <TimelineSeparator    >
 
-                                <Box className={classes.imgContainer}>
-                                    <img className={classes.img} src={icon1} alt="" />
-                                </Box>
-                              {  <TimelineConnector
-                                    className={classes.connector}
-                                />}
-                            </TimelineSeparator>
-                            <TimelineContent>
-                                <Typography
-                                    variant='h5'
-                                    contents='h5'
-                                    fontSize='18px'
-                                    fontWeight='bold'
+                                    <Box className={classes.imgContainer}>
+                                        <img className={classes.img} src={icon1} alt="" />
+                                    </Box>
+                                    {<TimelineConnector
+                                        className={classes.connector}
+                                    />}
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <Typography
+                                        variant='h5'
+                                        contents='h5'
+                                        fontSize={font}
+                                        fontWeight='bold'
 
-                                >Choose the Appliances</Typography>
-                                <Typography
-                                    variant='p'
-                                    contents='p'
-                                    fontSize='16px'
-                                    color='#757575'
-                                >Select the Service required</Typography>
-                            </TimelineContent>
-                        </TimelineItem>
-                        <TimelineItem>
-                            <TimelineSeparator    >
+                                    >Choose from our partners</Typography>
+                                    <Typography
+                                        variant='p'
+                                        contents='p'
+                                        fontSize={font1}
+                                        color='#757575'
+                                    >Verified Professionals</Typography>
+                                </TimelineContent>
+                            </TimelineItem>
+                            <TimelineItem>
+                                <TimelineSeparator    >
 
-                                <Box className={classes.imgContainer}>
-                                    <img className={classes.img} src={icon1} alt="" />
-                                </Box>
-                              {/*  <TimelineConnector
-                                    className={classes.connector}
-                                /> */}
-                            </TimelineSeparator>
-                            <TimelineContent>
-                                <Typography
-                                    variant='h5'
-                                    contents='h5'
-                                    fontSize='18px'
-                                    fontWeight='bold'
+                                    <Box className={classes.imgContainer}>
+                                        <img className={classes.img} src={icon3} alt="" />
+                                    </Box>
+                                    {/*  <TimelineConnector
+                                      className={classes.connector}
+                                  /> */}
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <Typography
+                                        variant='h5'
+                                        contents='h5'
+                                        fontSize={font}
+                                        fontWeight='bold'
 
-                                >Choose the Appliances</Typography>
-                                <Typography
-                                    variant='p'
-                                    contents='p'
-                                    fontSize='16px'
-                                    color='#757575'
-                                >Select the Service required</Typography>
-                            </TimelineContent>
-                        </TimelineItem>
-                    </Timeline>
+                                    >Hassle-free service</Typography>
+                                    <Typography
+                                        variant='p'
+                                        contents='p'
+                                        fontSize={font1}
+                                        color='#757575'
+                                    >24*7, verifed professionals</Typography>
+                                </TimelineContent>
+                            </TimelineItem>
+                        </Timeline>
+                    </Container>
                 </Box>
             </Box>
         </Box>
