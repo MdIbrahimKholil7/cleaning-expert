a0582d5615bb695b6fad9a14af42cb4b441f4375
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-08-25 12:16:47 +0600

    loader add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './components/Share/Header';
-import Home from './components/Home/Home';
+// import Home from './components/Home/Home';
+import React, { Suspense } from 'react';
+import loader from './assets/829.gif'
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles=makeStyles({
+  loaderContainer:{
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    height:'100vh'
+  },
+  loaderImg:{
+    width:'100px'
+  }
+})
 
 function App() {
+  const Home = React.lazy(() => import('./components/Home/Home'));
+  const classes=useStyles()
   return (
     <div className='app'>
       <Header />
-      <Home/>
+      <Suspense
+        fallback={<div
+          className={classes.loaderContainer}
+          
+        >
+          <img  className={classes.loaderImg} src={loader} alt='loader' />
+        </div>}
+      >
+        <Home />
+      </Suspense>
     </div>
   );
 }
